f22ada92329fabc33e9f5b4ddcdf08b66b3180d7
binod rai <br163406@gmail.com>
2025-08-28 23:41:15 +0545

    fetch songs according to mood done

--- backend/src/routes/song.routes.js
+++ backend/src/routes/song.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const songModel = require("../models/song.model")
+const songModel = require("../models/song.model");
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -10,19 +10,42 @@ router.post("/songs", upload.single("audio"), async (req, res) => {
   try {
     const fileData = await uploadFile(req.file);
     const song = await songModel.create({
-      title:req.body.title,
-      artist:req.body.artist,
-      audio:fileData.url,
-      mood:req.body.mood
-    })
+      title: req.body.title,
+      artist: req.body.artist,
+      audio: fileData.url,
+      mood: req.body.mood,
+    });
 
     res.json({
       message: "post sucessful",
-      song: song
+      song: song,
     });
-
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      message: "something went wrong",
+      error: error.message,
+    });
+  }
+});
+
+router.get("/songs", async (req, res) => {
+  try {
+      const { mood } = req.query;
+
+      const songs = await songModel.find({
+        mood: mood,
+      });
+
+      res.status(200).json({
+        message: "mood song fetched",
+        songs: songs
+      })
+
+      res.json({
+        message: "song according to mood fetched",
+        songs: songs,
+      });
+  } catch (error) {
     res.status(500).json({
       message: "something went wrong",
       error: error.message,
